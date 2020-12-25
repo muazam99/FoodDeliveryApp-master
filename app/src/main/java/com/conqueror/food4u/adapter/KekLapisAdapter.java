3e7ae360f0c3326ab90e421d6d793bee904c82e5
Muhammad Afiq BIn Mohd Murad <muhammadafiq99@users.noreply.github.com>
2020-12-24 19:28:56 -0800

    class kek lapis

--- app/src/main/java/com/conqueror/food4u/adapter/KekLapisAdapter.java
+++ app/src/main/java/com/conqueror/food4u/adapter/KekLapisAdapter.java
@@ -1,4 +1,75 @@
 package com.conqueror.food4u.adapter;
 
-public class KekLapisAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.conqueror.food4u.R;
+import com.conqueror.food4u.model.KekLapis;
+
+import java.util.List;
+
+public class KekLapisAdapter extends RecyclerView.Adapter<KekLapisAdapter.KekLapisViewHolder>{
+
+    Context context;
+    List<KekLapis> kekLapisList;
+
+
+
+    public KekLapisAdapter(Context context, List<KekLapis> kekLapisList) {
+        this.context = context;
+        this.kekLapisList = kekLapisList;
+    }
+
+    @NonNull
+    @Override
+    public KekLapisViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(context).inflate(R.layout.kek_lapis_row_item, parent, false);
+        return new KekLapisAdapter.KekLapisViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(KekLapisAdapter.KekLapisViewHolder holder, int position) {
+
+        holder.foodImage.setImageResource(kekLapisList.get(position).getImageUrl());
+        holder.name.setText(kekLapisList.get(position).getName());
+        holder.price.setText(kekLapisList.get(position).getPrice());
+        holder.rating.setText(kekLapisList.get(position).getRating());
+        holder.restorantName.setText(kekLapisList.get(position).getRestorantname());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return kekLapisList.size();
+    }
+
+
+    public static final class KekLapisViewHolder extends RecyclerView.ViewHolder{
+
+
+        ImageView foodImage;
+        TextView price, name, rating, restorantName;
+
+        public KekLapisViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            foodImage = itemView.findViewById(R.id.food_image);
+            price = itemView.findViewById(R.id.price);
+            name = itemView.findViewById(R.id.name);
+            rating = itemView.findViewById(R.id.rating);
+            restorantName = itemView.findViewById(R.id.restorant_name);
+
+
+
+        }
+    }
+
 }
